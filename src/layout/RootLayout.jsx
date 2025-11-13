1799e93d254ef9fe36039f569b7ed929a4ccadfd
Jony <jony378892@gmail.com>
2025-11-13 13:52:18 +0600

    Made some improvement inside rootlayout component

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router";
+import { Toaster } from "react-hot-toast";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 export default function RootLayout() {
   return (
-    <div className="flex flex-col min-h-screen font-poppins">
+    <div className="flex flex-col font-poppins">
+      <Toaster />
       <Navbar />
-      <section className="flex-1 mx-auto max-w-7xl">
+      <section className="flex-1 min-h-screen ">
         <Outlet />
       </section>
       <Footer />
